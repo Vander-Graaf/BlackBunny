aa299ca4ca23b82cdcf59ee80c7e02b2571d6100
Vander-Graaf <138441872+Vander-Graaf@users.noreply.github.com>
2024-08-01 22:42:35 +0600

    deploy

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -20,7 +20,7 @@ function HomePage({ setBasket }) {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("https://blackbunny-backend.onrender.com/products");
+        const response = await axios.get("http://127.0.0.1:5000/products");
         setProducts(response.data);
 
         const initialCounters = response.data.reduce((acc, product) => {
@@ -85,9 +85,9 @@ function HomePage({ setBasket }) {
     <>
       <SortButtons />
 
-      <div>
+      <div className="align-cards">
         {products.length > 0 ? (
-          <ul className="HomePage">
+          <ul className="Cards">
             {products.map((product) => (
               <div key={product._id} className="card">
                 <div className="image-align">
