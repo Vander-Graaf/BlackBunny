374d1e29aae42c575713c14d51a5f59eeb993ac1
VanderG-dev <VanderG.dev@gmail.com>
2025-07-18 22:51:29 +0600

    fix admin page

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure this matches the directory your platform is expecting
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://blackbunny-backend-o7if.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
